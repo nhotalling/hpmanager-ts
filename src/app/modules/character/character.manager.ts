85bed753082701545ae8039c247c5f88f709139e
Nick Hotalling <nick.hotalling@gmail.com>
2020-11-15 17:07:05 -0600

    Adds method docs, int parser

--- src/app/modules/character/character.manager.ts
+++ src/app/modules/character/character.manager.ts
@@ -7,15 +7,57 @@ import { CharacterHealth } from '../../models/character-health';
 import { DamageRequest } from '../../models/damage-request';
 
 export interface ICharacterManager {
+  /**
+   * Adds temporary HP to a character. Temp HP do not stack.
+   * @param name The character's name (case-insensitive)
+   * @param amount Amount of temporary HP to add. (Negative numbers allowed)
+   */
   addTempHp(name: string, amount: number): CharacterHealth;
+
+  /**
+   * Calculates and applies the specified damage to a character, taking into account
+   * vulnerability, immunity, and resistance.
+   * @param name The character's name (case-insensitive)
+   * @param damage Array of damage types and amount of damage to deal
+   */
   dealDamage(name: string, damage: Array<DamageRequest>): CharacterHealth;
+
+  /**
+   * Evaluates damage types and values against a character's defenses
+   * and returns the total damage dealt.
+   * @param damageRequest Array of damage types and amount of damage to deal
+   * @param defenses The character's defenses that may modify the damage dealt
+   */
   calculateDamage(
     damageRequest: Array<DamageRequest>,
     defenses: Array<Defense>
   ): number;
+
+  /**
+   * Applies calculated damage, taking temporay hit points into account.
+   * (Note this does not deal with unconsciousness/death)
+   * @param damage The amount of damage to apply
+   * @param originalHealth The health object to apply damage to
+   */
   applyDamage(damage: number, originalHealth: CharacterHealth): CharacterHealth;
+
+  /**
+   * Get a character by name
+   * @param name The character's name (case-insensitive)
+   */
   getCharacter(name: string): Character;
+
+  /**
+   * Gets a character's max HP, current HP, and temporary HP
+   * @param name The character's name (case-insensitive)
+   */
   getStatus(name: string): CharacterHealth;
+
+  /**
+   * Heals a character, up to their max HP value.
+   * @param name The character's name (case-insensitive)
+   * @param amount The amount of HP to recover
+   */
   heal(name: string, amount: number): CharacterHealth;
 }
 
@@ -32,15 +74,55 @@ export class CharacterManager implements ICharacterManager {
     this.characterHealthService = healthService;
   }
 
+  /**
+   * Adds temporary HP to a character. Temp HP do not stack.
+   * @param name The character's name (case-insensitive)
+   * @param amount Amount of temporary HP to add. (Negative numbers allowed)
+   */
+  addTempHp(name: string, amount: number): CharacterHealth {
+    var health = this.getCharacterHealth(name);
+    let newTempHp = 0;
+    amount = this.getInt(amount);
+
+    if (amount < 0) {
+      // Allowing user to send negative temp hp
+      // as a means to correct an error
+      newTempHp = health.tempHp + amount;
+      newTempHp = Math.max(0, newTempHp);
+    } else {
+      // Temp HP do not stack
+      newTempHp = Math.max(amount, health.tempHp);
+    }
+
+    health.tempHp = newTempHp;
+    this.characterHealthService.save(health);
+
+    return health;
+  }
+
+  /**
+   * Get a character by name
+   * @param name The character's name (case-insensitive)
+   */
   public getCharacter(name: string): Character {
     const character = this.characterService.getByName(name);
     return character;
   }
 
+  /**
+   * Gets a character's max HP, current HP, and temporary HP
+   * @param name The character's name (case-insensitive)
+   */
   public getStatus(name: string): CharacterHealth {
     return this.getCharacterHealth(name);
   }
 
+  /**
+   * Gets a characters health from the service (repo).
+   * If not found, it loads the character and calculates its
+   * max HP, then stores the health value for use during this session.
+   * @param name
+   */
   private getCharacterHealth(name: string): CharacterHealth {
     let currentHealth = this.characterHealthService.getCharacterHealth(name);
     if (currentHealth != null) {
@@ -64,39 +146,66 @@ export class CharacterManager implements ICharacterManager {
     return currentHealth;
   }
 
-  addTempHp(name: string, amount: number): CharacterHealth {
-    var health = this.getCharacterHealth(name);
-    let newTempHp = 0;
-
-    if (amount < 0) {
-      // Allowing user to send negative temp hp
-      // as a means to correct an error
-      newTempHp = health.tempHp + amount;
-      newTempHp = Math.max(0, newTempHp);
-    } else {
-      // Temp HP do not stack
-      newTempHp = Math.max(amount, health.tempHp);
-    }
-
-    health.tempHp = newTempHp;
-    this.characterHealthService.save(health);
-
-    return health;
+  /**
+   * Calculates and applies the specified damage to a character, taking into account
+   * vulnerability, immunity, and resistance.
+   * @param name The character's name (case-insensitive)
+   * @param damage Array of damage types and amount of damage to deal
+   */
+  dealDamage(name: string, damage: Array<DamageRequest>): CharacterHealth {
+    throw new Error('Method not implemented.');
+    // var originalHealth = GetCharacterHealth(name);
+    // // GetCharacterHealth has null checks for character, so we can assume character is not null
+    // var character = _characterService.GetCharacter(name);
+    // var damageTaken = CalculateDamage(damageRequest, character.Defenses);
+    // if (damageTaken == 0) {
+    //   return originalHealth;
+    // }
+    // var updatedHealth = ApplyDamage(damageTaken, originalHealth);
+    // _characterHealthService.Save(updatedHealth);
+    // return updatedHealth;
   }
 
-  dealDamage(name: string, damage: DamageRequest[]): CharacterHealth {
+  /**
+   * Evaluates damage types and values against a character's defenses
+   * and returns the total damage dealt.
+   * @param damageRequest Array of damage types and amount of damage to deal
+   * @param defenses The character's defenses that may modify the damage dealt
+   */
+  calculateDamage(
+    damageRequest: Array<DamageRequest>,
+    defenses: Array<Defense>
+  ): number {
     throw new Error('Method not implemented.');
   }
-  calculateDamage(damageRequest: DamageRequest[], defenses: Defense[]): number {
-    throw new Error('Method not implemented.');
-  }
+
+  /**
+   * Applies calculated damage, taking temporay hit points into account.
+   * (Note this does not deal with unconsciousness/death)
+   * @param damage The amount of damage to apply
+   * @param originalHealth The health object to apply damage to
+   */
   applyDamage(
     damage: number,
     originalHealth: CharacterHealth
   ): CharacterHealth {
     throw new Error('Method not implemented.');
   }
+
+  /**
+   * Heals a character, up to their max HP value.
+   * @param name The character's name (case-insensitive)
+   * @param amount The amount of HP to recover
+   */
   heal(name: string, amount: number): CharacterHealth {
     throw new Error('Method not implemented.');
+  }
+
+  private getInt(value: number): number {
+    const newValue = Math.floor(value);
+    if (isNaN(newValue)) {
+      throw new Error('Please provide a valid number');
+    }
+    return newValue;
   }
 }

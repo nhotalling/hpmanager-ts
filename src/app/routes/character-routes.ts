73cf6b587d9a6f4987c0aa91e9b580351544bff7
Nick Hotalling <nick.hotalling@gmail.com>
2020-11-14 22:10:10 -0600

    change inject to interface

--- src/app/routes/character-routes.ts
+++ src/app/routes/character-routes.ts
@@ -1,16 +1,16 @@
 import { Request, Response, Application } from 'express';
 import container from '../inversify.config';
 import { TYPES } from '../types';
-import { CharacterManager } from '../modules/character/character.manager';
+import { ICharacterManager } from '../modules/character/character.manager';
 
-const characterManager = container.get<CharacterManager>(
+const characterManager = container.get<ICharacterManager>(
   TYPES.CharacterManager
 );
 
 export class CharacterRoutes {
   public routes(app: Application): void {
     app.route('/character').get((req: Request, res: Response) => {
-      res.status(200).send({ hello: characterManager.test() });
+      res.status(200).send({ hello: characterManager.test() + ' interface' });
     });
 
     app.route('/character/:name').get((req: Request, res: Response) => {
